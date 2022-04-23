396c5666b81e568481efd05a2317d183c12295cf
lohsuan <angelalo88362@gmail.com>
2022-04-23 17:06:08 +0800

    connect to firebase on Android

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home '),
     );
   }
 }
